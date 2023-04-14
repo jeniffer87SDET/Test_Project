c00c53e6b4868687cc4b34e03757289e5c27a0ab
jeniffer87SDET <129709645+jeniffer87SDET@users.noreply.github.com>
2023-04-13 22:45:21 -0400

    Update Test_Class.java
    Added greeting method

--- src/test_Package/Test_Class.java
+++ src/test_Package/Test_Class.java
@@ -5,5 +5,8 @@ public class Test_Class {
 	public static void main (String args []) {
 		System.out.println("Test message...");
 	}
+	
+	public void greeting(){
+		system.out.println("Hello there...");
 
 }
